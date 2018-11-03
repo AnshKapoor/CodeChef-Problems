ab1cebd6778d42a0a322c7b72c2563afccbf3d39
Ansh Kapoor <32305554+AnshKapoor@users.noreply.github.com>
2018-11-03 23:31:42 +0530

    Solution to FCTRL2 problem code

---
+++ FCTRL2.java
@@ -0,0 +1,22 @@
+// problem link : https://www.codechef.com/problems/FCTRL2
+import java.math.*;
+import java.util.Scanner;
+ class smallFactorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s= new Scanner(System.in);
+		int t= s.nextInt();
+		for(int i=0;i<t;i++){
+			
+			int n= s.nextInt();
+			BigInteger b= new BigInteger("1");
+			
+			for(int j=1;j<=n;j++){
+				b=b.multiply(BigInteger.valueOf(j));
+			}
+			System.out.println(b);
+		
+	}
+       }
+}
